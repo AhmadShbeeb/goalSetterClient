02fe3e18d433559d4ca36c15432f751d78a6d333
GaM30v3r <ahmadshbeeb96@gmail.com>
2022-09-09 19:27:59 +0300

    init

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -5,7 +5,6 @@ const API_URL = '/api/users/'
 // Register User
 const register = async userData => {
   const response = await axios.post(API_URL, userData)
-  console.log(API_URL)
 
   if (response.data) localStorage.setItem('user', JSON.stringify(response.data))
 
