a5057a7f751400dc6c1540acc78f361888bd7d81
GaM30v3r <ahmadshbeeb96@gmail.com>
2022-09-15 15:54:52 +0300

    p

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const API_URL = '/api/users/'
-const API_URL = 'https://goalsetterapi.cyclic.app/api/users/'
+const API_URL = '/api/users/'
+// const API_URL = 'https://goalsetterapi.cyclic.app/api/users/'
 
 // Register User
 const register = async userData => {
